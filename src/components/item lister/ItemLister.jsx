7da460f0c8a2d4e2abc7e4574dcc10aa9a661cec
codeaali <mail.afzalalis@gmail.com>
2024-05-22 21:38:58 +0530

    updated the weekly view in itemLister

--- src/components/item lister/ItemLister.jsx
+++ src/components/item lister/ItemLister.jsx
@@ -10,20 +10,19 @@ function ItemLister(props) {
         { weeklydata.length > 0 && weeklydata.map((it,index)=>
         ( <div className="items">
         
-        <br />
+        
         <div className="items-container">
         {/* <BsFillCartCheckFill /> */}
 
-        <div className=""><p>{it['Total Planned Weekly']} </p></div>
+        <div className=""><p>Total planned: {it['Total Planned Weekly']} </p></div>
         <br />
+        <div className=""><p>Total completed: {it['Total Completed Weekly']}</p> </div>
         <br />
-        <div className=""><p>{it['Total Completed Weekly']}</p> </div>
+        <div className=""><p>Weekly completed: {it['Weekly Completed %']}</p> </div>
         <br />
-        <div className=""><p>{it['Weekly Completed %']}</p> </div>
+        <div className=""><p>Total rejected weekly: {it['Total Rejected Weekly']} </p></div>
         <br />
-        <div className=""><p>{it['Total Rejected Weekly']} </p></div>
-        <br />
-        <div className=""><p>{it['Weekly Rejected %']}</p> </div>
+        <div className=""><p>Weekly rejected: {it['Weekly Rejected %']}</p> </div>
         </div>
         </div>)
         
